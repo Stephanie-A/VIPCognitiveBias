97f00d993d7e94e341b3bee4c16a449bb689d2b4
Stephanie <almeidasmail@gmail.com>
2018-02-27 17:15:36 -0500

    Added dictionaries

---
+++ dicts.py
@@ -0,0 +1,22 @@
+hypothesis = "I think that "
+other = "I think that "
+
+confidence = {
+    hypothesis + ' 1': 10,
+    hypothesis + ' 2': 3,
+    hypothesis + ' 3': 2,
+    hypothesis + ' 4': 20,
+    hypothesis + ' 5': 40,
+    other + ' 1': 10,
+    other + ' 2': 3,
+    other + ' 3': 2,
+    other + ' 4': 20,
+    other + ' 5': 40,
+}
+
+questions = {
+    'location' : 'Why do you think it is in that location?',
+    'person' : 'Why do you think it is that person?',
+    'time' : 'Why do you think it is during that time period',
+    'other' : 'Why do you think it is so?'
+}
