c74314edc3cf46d61b6d12d0ac7ae475f9d73053
Rachel Zhong <3232558145@qq.com>
2018-03-07 14:16:34 -0500

    remove print statements

--- anchoringbias.py
+++ anchoringbias.py
@@ -7,7 +7,6 @@ def anchoringbias(focus, dict1): # pass in the area of focus
     if len(dict2) == 0:
         return "You have focused on location, person, and time. Do you have other aspects you'd like to focus on?";
     newFocus = dict2[random.randrange(0, len(dict2))];
-    print(dict2);
 
     return "You are focusing on " + focus + " too much." + " Do you have any hypothesis regarding " + newFocus;
 
